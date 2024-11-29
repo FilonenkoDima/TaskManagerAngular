eebc1de168571a05ddf2613555b04e376b3de7f9
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-29 11:28:29 +0200

    added project structure

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TasksListComponent } from "./tasks-list/tasks-list.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [TasksListComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+
+}
