c975efe004cbb4c93970656719ba661163f12eb4
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-29 16:30:31 +0200

    added task model

--- src/app/tasks/tasks-list/task-item/task-item.component.ts
+++ src/app/tasks/tasks-list/task-item/task-item.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Task } from '../../task.model';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
   imports: [],
   templateUrl: './task-item.component.html',
-  styleUrl: './task-item.component.css'
+  styleUrl: './task-item.component.css',
 })
 export class TaskItemComponent {
-
+  task = input.required<Task>();
 }
