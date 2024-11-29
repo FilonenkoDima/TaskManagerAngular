2c3729b54a6d3e3d09407860dc6f7008cd27026e
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-29 11:48:10 +0200

    added template to new task component

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
   imports: [],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
+  private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
+  onAddTasks(title: string, description: string) {
+    this.formEl()?.nativeElement.reset();
+  }
 }
