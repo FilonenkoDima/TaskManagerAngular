6ecd0e5495496926f4bbd9daa506c6eac3c72a88
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-29 12:02:03 +0200

    added template to task list component

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @Component({
   selector: 'app-tasks-list',
   standalone: true,
-  imports: [],
+  imports: [TaskItemComponent],
   templateUrl: './tasks-list.component.html',
-  styleUrl: './tasks-list.component.css'
+  styleUrl: './tasks-list.component.css',
 })
 export class TasksListComponent {
+  selectedFilter = signal<string>('all');
+  tasks = [];
 
+  onChangeTasksFilter(filter: string) {
+    this.selectedFilter.set(filter);
+  }
 }
