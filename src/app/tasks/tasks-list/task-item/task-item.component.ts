c481ed8ba8c8029ab1704e5bd8185078005219e0
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-29 17:02:06 +0200

    added task item template

--- src/app/tasks/tasks-list/task-item/task-item.component.ts
+++ src/app/tasks/tasks-list/task-item/task-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, input } from '@angular/core';
-import { Task } from '../../task.model';
+import { Component, computed, input } from '@angular/core';
+import { Task, TaskStatus } from '../../task.model';
 
 @Component({
   selector: 'app-task-item',
@@ -10,4 +10,34 @@ import { Task } from '../../task.model';
 })
 export class TaskItemComponent {
   task = input.required<Task>();
+  taskStatus = computed(() => {
+    switch (this.task().status) {
+      case 'OPEN':
+        return 'Open';
+      case 'IN_PROGRESS':
+        return 'Working on it';
+      case 'DONE':
+        return 'Completed';
+      default:
+        return 'Open';
+    }
+  });
+
+  onChangeTaskStatus(taskId: string, status: string) {
+    let newStatus: TaskStatus = 'OPEN';
+
+    switch (status) {
+      case 'open':
+        newStatus = 'OPEN';
+        break;
+      case 'in-progress':
+        newStatus = 'IN_PROGRESS';
+        break;
+      case 'done':
+        newStatus = 'DONE';
+        break;
+      default:
+        break;
+    }
+  }
 }
